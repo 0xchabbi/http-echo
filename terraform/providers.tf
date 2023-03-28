79dc44924de5b383bc11de3745f5a05aa0f20148
0xchabb <bilalchabbi@hotmail.com>
2023-03-28 14:11:00 +0200

    chnage provider and add tasks

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.30.0"
+      version = ">=3.30.0"
     }
-    azuredevops = {
-      source = "microsoft/azuredevops"
-      version = "~> 2.0"
-    }
+    # azuredevops = {
+    #   source = "microsoft/azuredevops"
+    #   version = "~> 2.0"
+    # }
   }
 }
 
