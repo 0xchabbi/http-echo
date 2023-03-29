da361096a6fc57d1e23687cdd8a85e14b2163e01
0xchabb <bilalchabbi@hotmail.com>
2023-03-29 13:37:30 +0200

    change version

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.30.0"
+      version = "=3.49.0"
     }
    azuredevops = {
       source = "microsoft/azuredevops"
-      version = ">=0.1.0"
+      version = "=0.4.0"
     }
   }
 }
